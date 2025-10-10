c8c965569fc61b279e2089ad6f3b2138c5d0f7e2
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-10-10 19:05:28 -0400

    feat: add initial structure for identity service with Maven wrapper and Spring Boot configuration

---
+++ services/identity/src/test/java/com/airbnb_clone_ms_web_iii/identity/IdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb_clone_ms_web_iii.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
