416f19148996f4270e6a207903114387d755510f
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-10-27 02:34:42 -0400

    feat: add home module and update listings mock with new properties

--- frontend/src/app/shared/shared-module.ts
+++ frontend/src/app/shared/shared-module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './ui/header/header.component';
 import { RouterModule } from '@angular/router';
+import { CardCarouselComponent } from './ui/card-carousel/card-carousel.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, CardCarouselComponent],
   imports: [
     CommonModule,
     RouterModule
