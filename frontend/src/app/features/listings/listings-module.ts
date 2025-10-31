93eb6f404e6236ab2a47c38e88c69390dd4714a5
IngFreddyRosales <739617@nur.edu.bo>
2025-10-31 17:03:28 -0400

    se agrego ele listingForm

--- frontend/src/app/features/listings/listings-module.ts
+++ frontend/src/app/features/listings/listings-module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListingsRoutingModule } from './listings-routing-module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccommodationsPageComponent } from '@listings/pages/listing-list/accommodations-page.component';
 import { ListingCardComponent } from '@listings/components/listing-card/listing-card.component';
 import { ListingDetailPageComponent } from './pages/listing-detail/listing-detail-page.component';
+
 @NgModule({
-  declarations: [AccommodationsPageComponent, ListingCardComponent, ListingDetailPageComponent],
-  imports: [FormsModule, CommonModule, ListingsRoutingModule],
+  declarations: [
+    AccommodationsPageComponent,
+    ListingCardComponent,
+    ListingDetailPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ListingsRoutingModule,
+  ],
 })
 export class ListingsModule {}
--- frontend/src/app/features/listings/listings-routing-module.ts
+++ frontend/src/app/features/listings/listings-routing-module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AccommodationsPageComponent } from '@listings/pages/listing-list/accommodations-page.component';
 import { ListingDetailPageComponent } from '@app/features/listings/pages/listing-detail/listing-detail-page.component';
+import { ListingFormPageComponent } from './pages/listing-form/listing-form-page.component'; // Importar el componente standalone
 
 const routes: Routes = [
+  {
+    path: 'form',
+    component: ListingFormPageComponent, // Usar el componente standalone aquí
+  },
   {
     path: ':id',
     component: ListingDetailPageComponent,
