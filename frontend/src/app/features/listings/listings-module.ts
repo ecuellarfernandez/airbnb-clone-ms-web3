0aae6e287df5f2e5e5b924df3a5e5024abb3023b
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-11-03 15:50:53 -0400

    feat: refactor listing card component to use app-card and emit click events

--- frontend/src/app/features/listings/listings-module.ts
+++ frontend/src/app/features/listings/listings-module.ts
@@ -5,8 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AccommodationsPageComponent } from '@listings/pages/listing-list/accommodations-page.component';
 import { ListingCardComponent } from '@listings/components/listing-card/listing-card.component';
 import { ListingDetailPageComponent } from './pages/listing-detail/listing-detail-page.component';
+import {SharedModule} from '@app/shared/shared-module';
+import { ListingsCoreComponent } from './core/listings-core/listings-core.component';
 @NgModule({
-  declarations: [AccommodationsPageComponent, ListingCardComponent, ListingDetailPageComponent],
-  imports: [FormsModule, CommonModule, ListingsRoutingModule],
+  declarations: [AccommodationsPageComponent, ListingCardComponent, ListingDetailPageComponent, ListingsCoreComponent],
+  imports: [FormsModule, CommonModule, ListingsRoutingModule, SharedModule],
 })
 export class ListingsModule {}
