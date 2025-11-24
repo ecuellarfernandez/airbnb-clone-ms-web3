2a4fca67a2f8f75c42828ed40708c5307f93d668
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-11-24 03:53:43 -0400

    moved

---
+++ frontend/src/app/shared/ui/nav-tabs/nav-tabs.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+export interface NavTab {
+  id: string;
+  label: string;
+  icon: 'home' | 'experience' | 'service';
+  isNew?: boolean;
+  path?: string;
+}
+
+@Component({
+  selector: 'app-nav-tabs',
+  standalone: false,
+  templateUrl: './nav-tabs.component.html',
+  styleUrl: './nav-tabs.component.scss'
+})
+export class NavTabsComponent {
+  @Input() tabs: NavTab[] = [];
+  @Input() activeTabId: string = '';
+  @Output() tabChange = new EventEmitter<string>();
+
+  onTabClick(tabId: string): void {
+    this.tabChange.emit(tabId);
+  }
+}
