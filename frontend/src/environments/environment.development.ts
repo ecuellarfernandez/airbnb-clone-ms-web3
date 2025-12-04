f0f942c72b771f37391dcfbede665ee23730f4fa
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-12-04 00:00:18 -0400

    feat: implement Cloudinary image upload functionality with dedicated module, service, and configuration.

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+// environment.development.ts
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:9000/api',
+    cloudinary: {
+        cloudName: 'dv0z9xhqb',
+        uploadPreset: 'airbbcitos',
+    },
+};
