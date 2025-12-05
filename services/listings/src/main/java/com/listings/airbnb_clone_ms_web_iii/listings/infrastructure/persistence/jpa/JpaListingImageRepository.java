2548b4b4604e1f1e71ff150d27af774506666a35
ecuellarfernandez <ecuellarfernandez@gmail.com>
2025-12-04 20:54:08 -0400

    feat: Add Cloudinary configuration and image storage adapter for image management

--- services/listings/src/main/java/com/listings/airbnb_clone_ms_web_iii/listings/infrastructure/persistence/jpa/JpaListingImageRepository.java
+++ services/listings/src/main/java/com/listings/airbnb_clone_ms_web_iii/listings/infrastructure/persistence/jpa/JpaListingImageRepository.java
@@ -4,9 +4,12 @@ import com.listings.airbnb_clone_ms_web_iii.listings.domain.model.ListingImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaListingImageRepository extends JpaRepository<ListingImage, UUID> {
     List<ListingImage> findByListingId(UUID listingId);
+    Optional<ListingImage> findByPublicId(String publicId);
+    void deleteByPublicId(String publicId);
 }
 
