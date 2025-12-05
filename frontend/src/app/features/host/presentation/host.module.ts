cf84371095f4405375b5b0d491965f1e2346b261
Bruno Cerusoli Gil <118586785+Bcerusoli@users.noreply.github.com>
2025-12-05 03:46:35 -0400

    SideBar en hosting

--- frontend/src/app/features/host/presentation/host.module.ts
+++ frontend/src/app/features/host/presentation/host.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HostRoutingModule } from './host-routing.module';
 import { HostMainPageComponent } from './host-main-page/host-main-page.component';
+import { HostSidebarComponent } from './components/host-sidebar/host-sidebar.component';
 import { SharedModule } from '@app/shared/shared-module';
 import { ConfirmationModalComponent } from '@app/shared/ui/confirmation-modal/confirmation-modal.component';
 
 @NgModule({
   declarations: [
-    HostMainPageComponent
+    HostMainPageComponent,
+    HostSidebarComponent
   ],
   imports: [
     CommonModule,
