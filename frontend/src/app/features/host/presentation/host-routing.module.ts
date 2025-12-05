823892f08828d580bfb3dff5b6a98b71fd8f2203
CrimsomDuke <grottordrynor@gmail.com>
2025-12-05 05:03:21 -0400

    temp

--- frontend/src/app/features/host/presentation/host-routing.module.ts
+++ frontend/src/app/features/host/presentation/host-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HostMainPageComponent } from './host-main-page/host-main-page.component';
 
+import { ListingDetailsPageComponent } from './listing-details-page/listing-details-page.component';
+
 const routes: Routes = [
   {
     path: '',
     component: HostMainPageComponent
+  },
+  {
+    path: 'listing-details/:id',
+    component: ListingDetailsPageComponent
   }
 ];
 
